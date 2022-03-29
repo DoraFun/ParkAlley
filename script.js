c041bafc4b8357ab6aa00b30f51f3cd175aa2e43
twep <228lol322kek@gmail.com>
2022-03-29 20:35:43 +0300

    added: js logic

---
+++ script.js
@@ -0,0 +1,6 @@
+arrowLeft = document.getElementById('arrow_left')
+arrowRight = document.getElementById('arrow_right')
+
+switchImg = document.getElementById('switch_img')
+switchHeader = document.getElementById('switch_header')
+switchText = document.getElementById('switch_text')
