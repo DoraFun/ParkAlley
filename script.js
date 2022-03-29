b379d4de97e2f460835004d367d16cc8aff4a99c
twep <228lol322kek@gmail.com>
2022-03-29 20:44:09 +0300

    adding

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ arrowRight = document.getElementById('arrow_right')
 switchImg = document.getElementById('switch_img')
 switchHeader = document.getElementById('switch_header')
 switchText = document.getElementById('switch_text')
+
+imgs = []
+
+document.addEventListener('click', )
